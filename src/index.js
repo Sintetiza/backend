69505381494126a1971fe807c965444e900401e1
BhryanS2 <bhryanstepenhen@gmail.com>
2022-05-17 21:23:30 -0300

    add port

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import Server from "./server.js";
+const port = process.env.PORT || 3000;
 
-const server = Server.listen(3000)
-  .on("listening", () => console.log(`Server is listening on port ${server.address().port}`))
+const server = Server.listen(port).on("listening", () =>
+  console.log(`Server is listening on port ${server.address().port}`)
+);
